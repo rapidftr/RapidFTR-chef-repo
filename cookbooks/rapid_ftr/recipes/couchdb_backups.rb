081a198f8cdac22e02af3081a96c6be88bb99dd7
Dave Cameron <dave.bc@gmail.com>
2011-01-04 23:54:46 -0500

    Starting of the replication works now.
    Special note to badh: if you had a face I would punch it.

--- cookbooks/rapid_ftr/recipes/couchdb_backups.rb
+++ cookbooks/rapid_ftr/recipes/couchdb_backups.rb
@@ -1,3 +1,11 @@
+%w{ rapidftr_form_section_production rapidftr_user_production rapidftr_sessions_production rapidftr_suggested_field_production rapidftr_contact_information_production rapidftr_child_production }.each do |db|
+  # this needs to be conditional, based on whether the database already exists
+  # http_request "rapidftr_form_section_production" do
+  #   action :put
+  #   url "http://admin:rapidftr_password@localhost:5984/#{db}/"
+  # end
+end
+
 cookbook_file "/etc/init.d/couchdb_backups" do
   source "couchdb_backups.init"
   owner "root"
