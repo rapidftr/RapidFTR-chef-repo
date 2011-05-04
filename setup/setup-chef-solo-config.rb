9ad1dbcc0c9e112e36d3fc9dec954bd05181bcf6
John D. Hume <duelin.markers@gmail.com>
2011-05-03 21:56:12 -0400

    Add prompt for app_server_fqdn to setup script.

--- setup/setup-chef-solo-config.rb
+++ setup/setup-chef-solo-config.rb
@@ -21,25 +21,41 @@ role_path "#{repo_root}/roles"
   END
 end
 
-puts "**************************************************************
-To generate chef-solo configuration, we need some information
-about SSL certificates. Please provide the locations where these
-files can be found. It's ok if the files aren't there yet, just
-be sure to put them there before you run chef-solo.
+puts "
+**************************************************************
+To generate chef-solo configuration, we need some information.
 
-If you decide you want to put them someplace else, you can edit
-#{node_attribute_file} after this script is finished.
+First we'll deal with SSL certificates. Please provide the locations
+where these files can be found. It's ok if the files aren't there yet,
+just be sure to put them there before you run chef-solo.
 "
 
 ssl_crt = Readline.readline("Enter the location of your certificate file (often ends in '.crt'):")
 ssl_key = Readline.readline("Enter the location of your certificate key file (often ends in '.key'):")
 
-puts "Writing #{node_attribute_file}..."
+puts "
+
+**************************************************************
+Now we need to know the public domain name of your application.
+Chef will guess this if you don't provide it. On this server it
+will probably guess
+    #{`hostname --fqdn`}
+If that's what you want, you can just hit enter.
+"
+
+fqdn = Readline.readline("Enter the publicly accessible domain name of your server:")
+
+puts "
+
+**************************************************************
+Writing #{node_attribute_file}..."
 File.open(node_attribute_file, 'w') do |file|
   file.write <<-END
 {
 	"rapid_ftr":{
-		"ssl_certificate": "#{ssl_crt}", "ssl_certificate_key": "#{ssl_key}"
+		"ssl_certificate": "#{ssl_crt}",
+		"ssl_certificate_key": "#{ssl_key}"
+		#{%(,"app_server_fqdn":"#{fqdn}") unless fqdn.empty?}
 	},
 	"passenger":{ "production":{ "bins_path": "/usr/bin" } },
 	"run_list":["role[default]"]
@@ -47,7 +63,8 @@ File.open(node_attribute_file, 'w') do |file|
   END
 end
 
-puts "**************************************************************
+puts "
+**************************************************************
 Chef should now be configured to run locally. If your SSL certificate
 files are in place, run
 sudo chef-solo
