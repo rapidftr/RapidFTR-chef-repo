c72c9c238e74bc83dbb1422138ca0154f7136fff
John Hume <duelin.markers@gmail.com>
2011-09-25 23:53:54 -0400

    Add xvfb service to the dev VM.

--- cookbooks/rapid_ftr/recipes/rails_developer.rb
+++ cookbooks/rapid_ftr/recipes/rails_developer.rb
@@ -7,7 +7,25 @@ include_recipe 'rapid_ftr::base'
 package 'xvfb'
 package 'firefox'
 
-service 'solr' do
+cookbook_file "/etc/init.d/xvfb" do
+  source "xvfb.init"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+bash 'export_DISPLAY_for_selenium' do
+  code <<-END
+  echo "\nexport DISPLAY=:99\n" >> /home/vagrant/.bashrc
+  END
+end
+
+service 'xvfb' do
+  supports :restart => true
+  action [:enable, :start]
+end
+
+service 'solr' do # not needed, but we may as well have it.
   supports :restart => true
   action [:disable]
 end
