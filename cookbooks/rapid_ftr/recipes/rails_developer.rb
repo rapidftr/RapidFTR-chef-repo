fa28607697d4e82abbf104e4d117cc5effdaf731
John Hume <duelin.markers@gmail.com>
2011-09-25 21:59:31 -0400

    Include xvfb and firefox packages in developer VM.

--- cookbooks/rapid_ftr/recipes/rails_developer.rb
+++ cookbooks/rapid_ftr/recipes/rails_developer.rb
@@ -2,9 +2,12 @@
 # (to jumpstart rails developers) before packaging
 # it up for distribution.
 
-include_recipe "rapid_ftr::base"
+include_recipe 'rapid_ftr::base'
 
-service "solr" do
+package 'xvfb'
+package 'firefox'
+
+service 'solr' do
   supports :restart => true
   action [:disable]
 end
