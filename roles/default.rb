16f685f0eadd3c33136cf68b27cc7273e0da7504
Subhas Dandapani <rdsubhas@thoughtworks.com>
2012-11-29 01:25:00 +0300

    Added Chef Librarian

--- roles/default.rb
+++ roles/default.rb
@@ -1,10 +1,9 @@
 name "default"
 description "RapidFTR server, with CouchDB, Solr, and Passenger-via-Nginx installed."
 run_list(
-  "recipe[apt]", # Should this be removed because it's debian-like-specific?
-  "recipe[build-essential]",
-  "recipe[erlang]",
-  "recipe[couchdb]",
   "recipe[git]",
-  "recipe[rapid_ftr]",
-  "recipe[passenger::daemon]")
+  "recipe[couchdb]",
+  "recipe[java]",
+  "recipe[passenger::daemon]",
+  "recipe[ssl]"
+)
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+RUBY_VERSION=1.8.7-p370
+RUBYGEMS_VERSION=1.8.24
+BUNDLER_VERSION=1.2.2
+
 echo "Updating apt package index..."
 sudo apt-get --yes update
 
 echo "Installing chef's required apt packages..."
 sudo apt-get --yes install build-essential wget ssl-cert libreadline6 libreadline6-dev openssl libssl-dev zlib1g zlib1g-dev
 
-echo "Installing Ruby"
-sudo apt-get --yes install ruby1.8 rubygems1.8 rdoc1.8 ruby1.8-dev
+echo "Installing ruby $RUBY_VERSION from source..."
+(
+cd /tmp
+wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-$RUBY_VERSION.tar.gz
+tar xzf ruby-$RUBY_VERSION.tar.gz
+cd ruby-$RUBY_VERSION
+./configure
+make
+sudo make install
+)
 
-echo "Installing chef..."
-sudo gem install chef --version 0.10.8 --no-rdoc --no-ri
+echo "Installing rubygems $RUBYGEMS_VERSION from source..."
+(
+cd /tmp
+wget http://production.cf.rubygems.org/rubygems/rubygems-$RUBYGEMS_VERSION.tgz
+tar zxf rubygems-$RUBYGEMS_VERSION.tgz
+cd rubygems-$RUBYGEMS_VERSION
+sudo ruby setup.rb --no-format-executable
+)
 
-# echo "Setting up chef-solo..."
-# sudo env SSL_CRT=$SSL_CRT SSL_KEY=$SSL_KEY FQDN=$FQDN CHEF_ROLE=$CHEF_ROLE ruby $CHEF_REPO_ROOT/setup/setup-chef-solo-config.rb
+echo "Installing bundler"
+sudo gem install bundler --version $BUNDLER_VERSION --no-rdoc --no-ri
+
+echo "Installing gems"
+sudo bundle install
+
+echo "Installing recipes"
+librarian-chef install
